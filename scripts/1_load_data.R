4fdf4a2beecb8a1626af6448289649016f498459
Chao Wang <emmawangchao@gmail.com>
2018-11-23 09:26:28 -0800

    add shiband style, write scripts in function, remove tukey analysis

--- scripts/1_load_data.R
+++ scripts/1_load_data.R
@@ -1,18 +1,35 @@
-# Required packages
+#! /usr/bin/env Rscript
+# 1_load_data.R
+# Team members: Miliban Keyim, Chao Wang, Kera Yucel
+#
+# This very first script loads raw data from the data folder and 
+# output clean data int to a csv file.
+#
+# Usage: Rscript 1_load_data.R
+
+
+# import libraries/packages
 library(readr)
 library(tidyverse)
 
-# Data analysis on greenhouse gas emission from 10 countries among 26 years
+# define main function
+main <- function(){
 
-data_GH <- read_csv("data/raw/2018-11-14_DSCI_522_project_UN-data_GH.csv", 
-                         col_types = cols(Year = col_character()))
-str(data_GH)
 
-# Rename column name `Country or Area`
+  # Data analysis on greenhouse gas emission from 10 countries among 26 years
+  data_GH <- read_csv("data/raw/2018-11-14_DSCI_522_project_UN-data_GH.csv", 
+                          col_types = cols(Year = col_character()))
+  str(data_GH)
 
-data_GH <- data_GH %>%
-  rename('Country' = 'Country or Area')
-data_GH$Country <- as.factor(data_GH$Country)
-str(data_GH)
-write_csv(data_GH, "data/clean_data_GH.csv")
+  # Rename column name `Country or Area`
 
+  data_GH <- data_GH %>%
+    rename('Country' = 'Country or Area')
+  data_GH$Country <- as.factor(data_GH$Country)
+  str(data_GH)
+  write_csv(data_GH, "data/clean_data_GH.csv")
+
+}
+
+# call main function
+main()
--- scripts/3_analyze_data.R
+++ scripts/3_analyze_data.R
@@ -1,22 +1,36 @@
-# Required libraries
+#! /usr/bin/env Rscript
+# 3_analyze_data.R
+# Team members: Miliban Keyim, Chao Wang, Kera Yucel
+#
+# Third script that reads the data from the first script, 
+# performs an ANOVA test and writes these numerical data to file in CSV format. 
+#
+# Usage: Rscript 3_analyze_data.R
+
+
+# import libraries/packages
 library(readr)
 library(broom)
 
-clean_data_GH <- read_csv("data/clean_data_GH.csv")
-str(clean_data_GH)
+# define main function
+main <- function(){
+  clean_data_GH <- read_csv("data/clean_data_GH.csv")
+  str(clean_data_GH)
 
-# ANOVA to detect if there is significant difference among the countries
+  # ANOVA to detect if there is significant difference among the countries
 
-aov_GH <-aov(Value ~ Country, data = clean_data_GH)
-summary(aov_GH)
-# there is significant difference in greenhouse emission among the countries
+  aov_GH <-aov(Value ~ Country, data = clean_data_GH)
+  summary(aov_GH)
+  # there is significant difference in greenhouse emission among the countries
+  
+  tidy_aov <- broom::tidy(aov_GH)
+  
+  #write ANOVA summary to .csv in results
+  write_csv(tidy_aov, "results/summarized_data.csv")
 
-tidy_aov <- broom::tidy(aov_GH)
-
-#write ANOVA summary to .csv in results
-write_csv(tidy_aov, "results/summarized_data.csv")
 
 
-# Which countries are difference compared to each other?
-TukeyHSD(aov_GH) #THIS PART NEEDS TO BE DISCUSSED.
+}
 
+# call main function
+main()
