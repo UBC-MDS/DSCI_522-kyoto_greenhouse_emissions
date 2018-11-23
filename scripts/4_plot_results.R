b8153d4c2fadd8ead6adf08f1cdb49c02caebe9a
Chao Wang <emmawangchao@gmail.com>
2018-11-23 11:37:42 -0800

    added another violin plot to show the mean and cis of each country

--- scripts/4_plot_results.R
+++ scripts/4_plot_results.R
@@ -37,6 +37,44 @@ main <- function(){
   ggsave("results/fig/GH_boxplot.png", plot =boxplot_result, width = 8, height = 4)
   
     
+
+
+
+  # get the ci and means by countries
+  GH_est <- boxplot_data %>% 
+    group_by(Country) %>% 
+    summarize(mean_GH = mean(Value),
+              n       = length(Value),
+              se      = sd(Value)/sqrt(n),
+              Value   = sum(Value))
+
+  GH_est_plot<- 
+    ggplot(boxplot_data,aes(fct_reorder(Country, Value), Value))+
+    geom_violin()+
+    theme_bw() +
+    labs(x = "Country", 
+        y = "Emission Value(kt)") +
+    scale_y_log10("Emissions Value(kt)",
+                  breaks = trans_breaks("log10", function(x) 10^x),
+                  labels = trans_format("log10", math_format(10^.x)))
+  
+  GH_est_plot <- GH_est_plot+
+     geom_point(data =GH_est, mapping = aes(x=Country, y=mean_GH),colour = "red")+
+     geom_errorbar(data =GH_est,mapping = aes(ymin = mean_GH+ qnorm(0.025)*se, 
+                                  ymax = mean_GH - qnorm(0.025)*se),
+                                  colour = "red",
+                                  width = 0.2)+
+    theme_bw() +
+    labs(x = "Country", 
+         y = "Emission Value(kt)") +
+    scale_y_log10("Emissions Value(kt)",
+                  breaks = trans_breaks("log10", function(x) 10^x),
+                  labels = trans_format("log10", math_format(10^.x)))
+
+
+  print(GH_est_plot)
+  # save violin plots
+  ggsave("results/fig/GH_est_plot.png", plot =GH_est_plot, width = 8, height = 4)
 }
 
 # call main function
