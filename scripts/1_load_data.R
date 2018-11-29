481ffc1e8a5aae2709ff37c208b7be68706f89f6
Mihriban Keyim <milibankeyim@gmail.com>
2018-11-28 20:25:27 -0800

    Created makefile and fixed 2 script errors in script 1 and script 2 along with run_all file and report

--- scripts/1_load_data.R
+++ scripts/1_load_data.R
@@ -2,7 +2,7 @@
 # 1_load_data.R
 # Team members: Miliban Keyim, Chao Wang, Kera Yucel
 #
-# This very first script loads raw data from the data folder and 
+# This very first script loads raw data from the data folder and
 # output clean data int to a csv file.
 #
 # Usage: Rscript scripts/1_load_data.R data/raw/2018-11-14_DSCI_522_project_UN-data_GH.csv data/data_GH.csv
@@ -22,7 +22,7 @@ main <- function(){
 
 
   # Data analysis on greenhouse gas emission from 10 countries among 26 years
-  data_GH <- read_csv(input, 
+  data_GH <- read_csv(input,
                           col_types = cols(Year = col_character()))
   str(data_GH)
 
